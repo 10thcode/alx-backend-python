89f3c78275d753be8e2e6de671cc5589203fca2f
10thcode <attabadukwabena@gmail.com>
2024-01-12 02:45:48 +0000

    Add 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+Define add function
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    Adds two floating numbers
+
+    Args:
+        a [float]: first number
+        b [float]: second number
+
+    Return:
+        sum of a and b
+    '''
+    return a + b
