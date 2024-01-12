8ff97d9950c9589391f7fa665de48d72765251de
10thcode <attabadukwabena@gmail.com>
2024-01-12 03:06:57 +0000

    Add 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+Define to_str function
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    Gets string representation of a floating number
+
+    Args:
+        n [float]: the number
+
+    Return:
+        [str]: the string representation of n
+    '''
+    return str(n)
