6b08d3483167d20dfaeca710b9373aa9ca895e73
10thcode <attabadukwabena@gmail.com>
2024-01-12 05:58:32 +0000

    Add 101-safely_get_valu

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''
+Defines safely_get_value function
+'''
+from typing import Mapping, Any, Union, TypeVar
+
+
+def safely_get_value(dct: Mapping,
+                     key: Any,
+                     default:
+                     Union[TypeVar('T'),
+                           None] = None) -> Union[Any, TypeVar('T')]:
+    '''
+    Gets a value from a dict
+
+    Args:
+        dct: the dict
+
+    Return:
+        value from a dict related to key or None
+    '''
+    if key in dct:
+        return dct[key]
+    else:
+        return default
