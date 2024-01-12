fb680a4e16f0afa9eaf0c8c8e1991ae19901041f
10thcode <attabadukwabena@gmail.com>
2024-01-12 03:00:10 +0000

    Add 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+Define function floor
+'''
+import math
+
+
+def floor(n: float) -> float:
+    '''
+    Get the floor of a number
+
+    Args:
+        n [float]: the number
+
+    Return:
+        [float]: the floor of n
+    '''
+    return math.floor(n)
